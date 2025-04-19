095ab20c7470772ea9f81b795653a2ec6bff0647
SARANG SURESH <sureshsarang44@gmail.com>
2025-04-19 12:04:33 +0530

    Update counter.c

--- TIMER0/counter.c
+++ TIMER0/counter.c
@@ -1,1 +1,28 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 17 April, 2025, 9:39 AM
+ *makig a delay of 10 ms*/
+
+#define F_CPU 8000000UL
+#include <xc.h>
+#include <avr/io.h>
+#include <util/delay.h>
+int i=0;
+
+void main(void) {
+    DDRB = 0Xff;
+    PORTB = 0x00;
+    TCCR0 = 0X07;
+    while(1){
+        if(TCNT0 == 1){
+            PORTB ^= (1<<7);
+            TCNT0 =0;
+        }
+    
+    }
+    
+  return;
+}
 
